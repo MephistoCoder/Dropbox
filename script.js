c78a5243c5958ba9106a68224ce430951b191eed
Bogdan <45145944+MephistoCoder@users.noreply.github.com>
2019-03-26 18:55:54 +0200

    Add
    add dropbox
    
    Co-Authored-By: Bogdan <mephistocoder@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,14 @@
+  var img = [...document.getElementsByClassName('main__gallery_img')];
+console.log(img);
+img.forEach(function(item) {
+  // item.onclick = ()=> toggle(item);
+//     item.onclick = ()=> {if ( display === true ) {
+//   $( "selector" ).show();
+// } else if ( display === false ) {
+//   $( "selector" ).hide();
+// }}
+
+})
+ // function toggle(el) {
+//   el.className = (el. == 'none') ? '' : 'none'
+}
