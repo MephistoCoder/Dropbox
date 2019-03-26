88705f40e8d9e94a115bb972a6fa4559b79b3a8c
Bogdan <45145944+MephistoCoder@users.noreply.github.com>
2019-03-26 22:18:51 +0200

    Фикс клика на изображение
    Коректное отображение при клике на изображение в галерее, фикс мелких неточностей
    
    Co-Authored-By: Bogdan <mephistocoder@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-
-var img = [...document.getElementsByClassName('main__gallery_img')];
-var block = [...document.getElementsByClassName('block_hover_circle')]
+var img = [...document.getElementsByClassName('main__gallery_img')];    // массив изображений с галереи
+var block = [...document.getElementsByClassName('block_hover_circle')]  // массив галочек на изображениях
 img.forEach(function(item, i) {
-  item.onclick = ()=> {
-    togle(item);
-    show(block[i]);
+  item.onclick = ()=> {                                                 // обработка клика на изображение 
+    toggle(item, 0.6, 1);
+    toggle(block[i], 0, 1);
   }
 });
-  function togle(el) {
-   el.style.opacity = (el.style.opacity == 1) ? 0.6 : 1;
-}
-function show(el) {
-  el.style.opacity = (el.style.opacity == 0) ? 1 : 0;
+  function toggle(el, max, min) {                                       // функция для изменения прозрачности элемента
+   el.style.opacity = (el.style.opacity == max) ? min : max;
 }
